7daa99944aeefa778cba34c2fb42adfe435b7cc9
Anton <antonr1@bk.ru>
2022-07-12 22:08:02 +0300

    add new pages

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../../pages/main-page/main-page';
+
+type AppProps = {
+  placesCount: string;
+}
+
+function App({placesCount} : AppProps): JSX.Element {
+  return <Main placesCount={placesCount}/>;
 }
 
 export default App;
